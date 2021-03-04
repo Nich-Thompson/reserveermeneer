8b58be33f45a924b98c35d39d3f7f359bfed3916
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-03-04 13:04:43 +0100

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -18,8 +19,13 @@ class UserSeeder extends Seeder
         //
         $admin = User::create([
             'name' => "admin",
-            'email' => 'admin@inspectietool.nl',
+            'email' => 'admin@gmail.nl',
             'password' => Hash::make('password')
         ]);
+        /*DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);*/
     }
 }
