edcba407111247e2a3cf392ea78a424750c14808
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-03-04 17:38:23 +0100

    Changed login/logout buttons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,7 +24,7 @@
             <!-- Settings Dropdown -->
             @auth()
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
+                {{--<x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ Auth::user()->name }}</div>
@@ -48,11 +48,29 @@
                             </x-dropdown-link>
                         </form>
                     </x-slot>
-                </x-dropdown>
+                </x-dropdown>--}}
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-dropdown-link :href="route('logout')"
+                                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                        {{ __('Log out') }}
+                    </x-dropdown-link>
+                </form>
             </div>
             @else
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                    <form method="GET" action="{{ route('login') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('logout')"
+                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                            {{ __('Log in') }}
+                        </x-dropdown-link>
+                    </form>
+                    {{--<a href="{{ route('login') }}" class="text-sm text-gray-700">Log in</a>--}}
                 </div>
             @endauth
 
