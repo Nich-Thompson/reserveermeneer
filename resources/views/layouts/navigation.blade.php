e6a2c1cbdb73109159f85129cf7bfe4a45139ffc
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-03-04 18:16:20 +0100

    removed dashboard for guest

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    @auth
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Dashboard') }}
+                        </x-nav-link>
+                    @endauth
                     <x-nav-link :href="route('getEventIndex')" :active="request()->routeIs('getEventIndex')">
                         {{ __('Evenementen') }}
                     </x-nav-link>
