63f3c7a9fc8cf56eefe5e6a181311f0ae941c7ca
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-03-06 14:45:46 +0100

    Update reserve.blade.php

--- resources/views/event/reserve.blade.php
+++ resources/views/event/reserve.blade.php
@@ -1,47 +1,38 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Reserveren') }}
+        </h2>
+    </x-slot>
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Nunito';
-        }
-    </style>
-</head>
-<body>
-<!-- if validation in the controller fails, show the errors -->
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <strong>De ingevoerde data is niet juist.</strong><br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ route('postEventReserve', 'id' ) }}" method="post" enctype="multipart/form-data">
+                        <!-- Add CSRF Token -->
+                        @csrf
+                        <div class="form-group">
+                            <label>Product Name</label>
+                            <input type="text" class="form-control" name="img_name" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="file" name="file" required>
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-@endif
-
-<div>
-
-    <form action="{{ route('postEventReserve', 'id' ) }}" method="post" enctype="multipart/form-data">
-        <!-- Add CSRF Token -->
-        @csrf
-        <div class="form-group">
-            <label>Product Name</label>
-            <input type="text" class="form-control" name="img_name" required>
-        </div>
-        <div class="form-group">
-            <input type="file" name="file" required>
-        </div>
-        <button type="submit">Submit</button>
-    </form>
-
-</div>
-</body>
-</html>
+</x-app-layout>
