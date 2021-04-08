91db84725a7b898231c598d032c13e4cefcc57bb
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-04-08 21:09:24 +0200

    Film reservation validation

--- app/Http/Requests/ReserveFilmRequest.php
+++ app/Http/Requests/ReserveFilmRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\FilmSeat;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ReserveFilmRequest extends FormRequest
@@ -24,7 +25,35 @@ class ReserveFilmRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+            'postal_code' => 'required',
+            'city' => 'required',
+            'seat_id' => 'required|gt:0',
         ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Naam mag niet leeg zijn.',
+            'email.required' => 'E-mail mag niet leeg zijn.',
+            'address.required' => 'Adres mag niet leeg zijn.',
+            'postal_code.required' => 'Postcode mag niet leeg zijn.',
+            'city.required' => 'Stad mag niet leeg zijn.',
+            'seat_id.required' => 'Kies een stoel.',
+            'seat_id.gt' => 'Kies een stoel.'
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $filmSeat = FilmSeat::find($this->seat_id);
+            if ($filmSeat) {
+                $validator->errors()->add('field', 'Deze stoel is al bezet.');
+            }
+        });
     }
 }
