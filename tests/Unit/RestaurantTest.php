f87b1641d18fdd866eedd667ee85b968e533cf32
Wesley Salimans <wlt.salimans@student.avans.nl>
2021-04-09 17:08:59 +0200

    eerste tests

--- tests/Unit/RestaurantTest.php
+++ tests/Unit/RestaurantTest.php
@@ -2,12 +2,57 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Http\Requests\ReserveRestaurantRequest;
+use Faker\Factory;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Validator;
 
 class RestaurantTest extends TestCase
 {
-    public function test_example()
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
     }
+
+    /**
+     * @dataProvider provideValidData
+     *
+     */
+    public function test_valid_firstname(array $data)
+    {
+        $request = new ReserveRestaurantRequest();
+        $validator = Validator::make($data, $request->rules());
+
+        $this->assertTrue($validator->passes());
+//        $this->assertTrue(true);
+    }
+
+    public function provideValidData(): array
+    {
+        $faker = Factory::create(Factory::DEFAULT_LOCALE);
+
+        return [
+            [[
+                "date" => "02-06-2021",
+                "time" => '17:00',
+                "firstname" => 'Wesley',
+                "lastname" => 'salimans',
+                "email" => 'wesley@gmail.com',
+                "address" => 'Bloemerstraat 17',
+                "postal_code" => '6031NV',
+                "city" => 'Nederweert',
+            ]],
+            [[
+                "date" => "02-06-2021",
+                "time" => '17:00',
+                "firstname" => $faker->firstName,
+                "lastname" => $faker->lastName,
+                "email" => $faker->email,
+                "address" => $faker->address,
+                "postal_code" => $faker->postcode,
+                "city" => $faker->city,
+            ]],
+        ];
+    }
+
 }
